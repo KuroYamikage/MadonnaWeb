0d4fbd7b33ba20372984bc8fef3dba2c6a0c6487
KuroYamikage <denzeledward1110@gmail.com>
2022-12-12 10:26:52 +0800

    Redirect to HomeFixed

--- Home/templates/index.php
+++ Home/templates/index.php
@@ -36,14 +36,14 @@
       <div class="header_section">
          <div class="container-fluid">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-               <div class="logo"><a href="index.html"><img src="{% static 'images/logo2.png' %}"></a></div>
+               <div class="logo"><a href="{%url 'index'%}  "><img src="{% static 'images/logo2.png' %}"></a></div>
                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
                </button>
                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav mr-auto">
                      <li class="nav-item">
-                        <a class="nav-link" href="index.html">Home</a>
+                        <a class="nav-link" href="{%url 'index'%}">Home</a>
                      </li>
                      <li class="nav-item">
                         <a class="nav-link" href="services.html">Reservations</a>
