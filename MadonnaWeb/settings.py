4fd9572a9464a4712b654ee696f299597bda0c66
jcesguerra <jcesguerra@student.apc.edu.ph>
2022-12-31 18:16:10 +0800

    Added "User" table to database

--- MadonnaWeb/settings.py
+++ MadonnaWeb/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Home.apps.HomeConfig'
+    'Home.apps.HomeConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila'
 
 USE_I18N = True
 
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    middle_initial = models.CharField(max_length=1)
+    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+
+    age = models.IntegerField()
+    sex = models.CharField(max_length=1)
+    birthday = models.DateField()
+    
+    password = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
