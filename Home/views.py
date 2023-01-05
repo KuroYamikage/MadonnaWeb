07b74d5b107815379eec4f14fdc0df95904dbffa
KuroYamikage <denzeledward1110@gmail.com>
2023-01-05 21:24:10 +0800

    JS file update

--- Home/views.py
+++ Home/views.py
@@ -15,10 +15,13 @@ def about(request):
 def reservations(request):
   reserve = Reservations.objects.all().values()
   template = loader.get_template('reservation.php')
-  context ={
-    'reserve' : reserve,
-  }
-  return HttpResponse(template.render(context,request))
+  reserve_html = []
+  for instance in Reservations.objects.all():  # it's not serialization, but extracting of the useful fields
+      reserve_html.append({'pk': instance.reservationID, 'fname': instance.firstname, 'lname': instance.lastname, 
+      'date': instance.date, 'dp': instance.downpayment, 'total': instance.totalPayment, 'bal': instance.balance
+      , 'stat': instance.status})
+  reserve_dic = {'reserve': reserve, 'ac_tab_n': 'ac_tab', 'reserve_html': reserve_html}
+  return render(request, 'reservation.php', reserve_dic)
 
 def sample(request):
     reserve = Reservations.objects.all().values()
@@ -29,7 +32,7 @@ def sample(request):
         'date': instance.date, 'dp': instance.downpayment, 'total': instance.totalPayment, 'bal': instance.balance
         , 'stat': instance.status})
     reserve_dic = {'reserve': reserve, 'ac_tab_n': 'ac_tab', 'reserve_html': reserve_html}
-    return HttpResponse(template.render(request, template, reserve_dic))
+    return render(request, 'test.php', reserve_dic)
 
 def add(request):
   template = loader.get_template('add.php')
