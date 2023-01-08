3fd86b3e877847791712b69d1c567beaa0f458f9
KuroYamikage <denzeledward1110@gmail.com>
2023-01-08 22:59:07 +0800

    Used Class Based Views and Fixed reservations

--- Home/urls.py
+++ Home/urls.py
@@ -4,10 +4,12 @@ from django.urls import include, path
 
 urlpatterns = [
     
-    path('', views.index, name='index'),
-    path('about', views.about, name='about'),
-    path('sample', views.sample, name='sample'),
-    path('add/', views.add, name='add'),
+    path('', views.indexView.as_view(), name='index'),
+    path('about', views.aboutView.as_view(), name='about'),
+    path('sample', views.sampleView.as_view(), name='sample'),
+    path('add/', views.addView.as_view(), name='add'),
     path('add/addrecord/', views.addrecord, name='addrecord'),
-    path('reserve', views.reservations, name='reserve'),
+    path('reserve', views.reserveView.as_view(), name='reserve'),
+    path('reservation/<int:pk>', views.moreDetailView.as_view(),name = 'reservation'),
+    path('reservation/new', views.newReserve.as_view(), name='reservation.new'),
 ]
