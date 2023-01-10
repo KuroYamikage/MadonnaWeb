ed4878cc34e77746b78681379a9b03f7d6646407
KuroYamikage <denzeledward1110@gmail.com>
2023-01-10 21:57:14 +0800

    Edited Login Page

--- users/views.py
+++ users/views.py
@@ -19,4 +19,4 @@ def register(request):
         form = UserRegistrationForm()
 
     context = {'form': form}
-    return render(request, 'users/register.html', context)
+    return render(request, 'users/register.php', context)
