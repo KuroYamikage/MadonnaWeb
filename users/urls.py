a708427a95c794423d96f5835bde984990614afb
KuroYamikage <denzeledward1110@gmail.com>
2023-01-16 10:37:53 +0800

    Midterm Updates

---
+++ users/urls.py
@@ -0,0 +1,19 @@
+from . import views
+from django.contrib import admin
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.StaffLoginView.as_view(), name='login'),
+    path('logout/', views.StaffLogoutView.as_view(), name='logout'),
+    path('staff/', views.reserveListView.as_view(), name='main'),
+    path('blog/', views.viewBlogs.as_view(), name='blog'),
+    path('blog/new', views.newBlog.as_view(), name='blog.add'),
+    path('blog/delete/<int:pk>', views.deleteBlogs.as_view(), name='blog.delete'),
+    path('blog/update/<int:pk>', views.updateBlogs.as_view(), name='blog.update'),
+    path('reservation/new', views.newReserveStaff.as_view(), name='reservation.new.staff'),
+    path('reservation/delete/<int:pk>', views.deleteReservation.as_view(), name='reservation.delete')
+
+]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,88 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from .forms import UserRegistrationForm
+from django.views.generic import CreateView, TemplateView,ListView,     DetailView, UpdateView
+from Home.models import Reservations, Blog
+from Home.forms import ReservationsForm, BlogForms
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
-def home(request):
-    return render(request, 'users/home.html')
+
+#Account Management
+class StaffLoginView(LoginView):
+  template_name = 'users/login.php'
+  redirect_authenticated_user= True
+
+class StaffLogoutView(LoginRequiredMixin,LogoutView):
+  template_name = 'users/logout.php'
+  login_url = "login"
+
+
+# Reservation Class
+class reserveListView(LoginRequiredMixin,ListView):
+  model = Reservations
+  context_object_name = 'reserve'
+  template_name = 'users/home.php'
+  login_url = "login"
+
+class newReserveStaff(LoginRequiredMixin,CreateView):
+  model = Reservations
+  form_class = ReservationsForm
+  success_url = 'main'
+  template_name = 'users/reservations_form.php'
+  login_url = "login"
+
+class deleteReservation(LoginRequiredMixin,DeleteView):
+  model= Reservations
+  context_object_name = 'reserve'
+  success_url = 'main'
+  template_name = 'users/delete_reservation.php'
+  login_url = "login"
+
+# Blogs 
+class viewBlogs(LoginRequiredMixin, ListView):
+    model = Blog
+    context_object_name = 'blog'
+    template_name = 'blog.php'
+    login_url = "login"
+
+class newBlog(LoginRequiredMixin, CreateView):
+  model = Blog
+  form_class = BlogForms
+  success_url ='/blog'
+  template_name = 'new_Blog.php'
+  login_url = "login"
+
+class viewBlogs(LoginRequiredMixin, ListView):
+  model = Blog
+  context_object_name = 'blog'
+  template_name = 'blog.php'
+  login_url = "login"
+
+class deleteBlogs(LoginRequiredMixin, DeleteView):
+  model= Blog
+  success_url = '/blog'
+  template_name = 'delete_blog.php'
+  login_url = "login"
+class updateBlogs(LoginRequiredMixin, UpdateView, SuccessMessageMixin):
+  model = Blog
+  form_class = BlogForms
+  success_message = 'List succcesfully edited'
+  success_url = 'blog'
+  template_name = 'edit_blog.php'
+  login_url = "login"
+
+class detaliBlog(DetailView):
+    model = Blog
+    context_object_name = 'blog'
+    template_name = 'blog_view.php'
+
+
 
 def register(request):
     if request.method == 'POST':
