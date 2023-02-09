f61c1fabbd48979212ed5d41429c63b79c9598fd
KuroYamikage <denzeledward1110@gmail.com>
2023-02-09 10:03:05 +0800

    Reservation Model done

--- Reservation/forms.py
+++ Reservation/forms.py
@@ -5,18 +5,14 @@ from .models import Reservations
 class ReservationForm(forms.ModelForm):
     class Meta:
         model = Reservations
-        fields = ('firstname', 'lastname', 'date', 'downpayment', 'totalPayment',
+        fields = ( 'date', 'downpayment', 'totalPayment',
             'balance', 'status')
         widgets = {
             'date' : forms.DateInput(attrs={'class': 'form-control'}),
-            'firstname' : forms.TextInput(attrs={'class' : 'form-control'}),
-            'lastname' : forms.TextInput(attrs={'class' : 'form-control'}),
-            'downpayment' : forms.TextInput(attrs={'class' : 'form-control'}),
             'totalPayment' : forms.TextInput(attrs={'class' : 'form-control'}),
             'balance' : forms.TextInput(attrs={'class' : 'form-control'}),
             'status' : forms.TextInput(attrs={'class' : 'form-control'}),
         }
         labels = {
-            'firstname' : 'First Name',
 
         }
--- Reservation/models.py
+++ Reservation/models.py
@@ -3,12 +3,31 @@ from django.db import models
 # Create your models here.
 
 class Reservations(models.Model):
-  firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
   reservationID = models.BigAutoField(primary_key=True)
   date = models.DateField()
+  time = models.TimeField()
+  discounted = models.BigIntegerField()
+  checkIn = models.DateField()
+  checkOut=models.DateField()
   downpayment = models.DecimalField(decimal_places=3, max_digits=10)
   totalPayment = models.DecimalField(decimal_places=3, max_digits=10)
   balance = models.DecimalField(decimal_places=3, max_digits=10)
   status = models.CharField(max_length=3)
+  
+class Customer(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  contactNumber = models.CharField(max_length=12)
+
+
+class Discount(models.Model):
+  discountCode= models.CharField(max_length=15)
+  discountPercentage = models.DecimalField(decimal_places=3, max_digits=3)
+  discountExpiration = models.DateTimeField()
+
+class Facility(models.Model):
+  facilityName = models.CharField(max_length=255)
+  facilityPrice = models.DecimalField(decimal_places=3, max_digits=6)
+  
+
   
