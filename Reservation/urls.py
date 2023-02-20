efcf6717db8299abe161b4131e190db13aa07372
jcesguerra <jcesguerra@student.apc.edu.ph>
2023-02-20 10:55:53 +0800

    Declared models and pandas/matplotlib deps

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -3,14 +3,20 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('test/', views.test,name='test'),
-    path('reserve/', views.reserveView.as_view(), name='reserve'),
-    path('reservation/<int:pk>', views.moreDetailView.as_view(),name = 'reservation'),
-    path('reservation/new', views.newReserve.as_view(), name='reservation.new'),
-    path('reservation/edit/<int:pk>', views.updateReserve.as_view(), name="reservation.edit"),
-    path('reservation/delete/<int:pk>', views.deleteReservation.as_view(), name='reservation.delete'),
-    path('staff/reserve', views.newReserveStaff.as_view(), name='reserve.staff.new'),
-    path('customer/new', views.newCustomer.as_view(), name='customer.new')
-
-
+    path("test/", views.test, name="test"),
+    path("reserve/", views.reserveView.as_view(), name="reserve"),
+    path("reservation/<int:pk>", views.moreDetailView.as_view(), name="reservation"),
+    path("reservation/new", views.newReserve.as_view(), name="reservation.new"),
+    path(
+        "reservation/edit/<int:pk>",
+        views.updateReserve.as_view(),
+        name="reservation.edit",
+    ),
+    path(
+        "reservation/delete/<int:pk>",
+        views.deleteReservation.as_view(),
+        name="reservation.delete",
+    ),
+    path("staff/reserve", views.newReserveStaff.as_view(), name="reserve.staff.new"),
+    path("customer/new", views.newCustomer.as_view(), name="customer.new"),
 ]
--- Reservation/views.py
+++ Reservation/views.py
@@ -3,58 +3,75 @@ from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import CreateView, TemplateView,ListView, DetailView, UpdateView
+from django.views.generic import (
+    CreateView,
+    TemplateView,
+    ListView,
+    DetailView,
+    UpdateView,
+)
 from django.contrib.auth import login, authenticate
 from Reservation.models import Reservations, Customer
 from Reservation.forms import ReservationForm, CustomerForm
 from django.contrib.messages.views import SuccessMessageMixin
+
+
 # Create your views here.
 def test(request):
-    return HttpResponse('this is a test')
+    return HttpResponse("this is a test")
+
 
 class reserveView(ListView):
-  model = Reservations
-  context_object_name = 'reserve'
-  template_name='reservation.php'
+    model = Reservations
+    context_object_name = "reserve"
+    template_name = "reservation.php"
+
 
 class newReserve(CreateView):
-  model = Reservations
-  form_class = ReservationForm
-  success_url = '/reserve'
-  template_name = 'reservation_form_Customer.php'
+    model = Reservations
+    form_class = ReservationForm
+    success_url = "/reserve"
+    template_name = "reservation_form_Customer.php"
+
 
 class newCustomer(CreateView):
-  model= Customer
-  form_class = CustomerForm
-  success_url = '../reservation/new'
-  template_name = 'customer_form_Customer.php'
+    model = Customer
+    form_class = CustomerForm
+    success_url = "../reservation/new"
+    template_name = "customer_form_Customer.php"
+
+
 class moreDetailView(DetailView):
-  model = Reservations
-  context_object_name = 'reserve'
-  template_name = 'test_reserve.php'
+    model = Reservations
+    context_object_name = "reserve"
+    template_name = "test_reserve.php"
 
-class reserveListView(LoginRequiredMixin,ListView):
-  model = Reservations
-  context_object_name = 'reserve'
-  template_name = 'users/templates/users/home.php'
-  login_url = "login"
 
-class newReserveStaff(LoginRequiredMixin,CreateView):
-  model = Reservations
-  form_class = ReservationForm
-  success_url = 'main'
-  template_name = 'reservations_form.php'
-  login_url = "login"
+class reserveListView(LoginRequiredMixin, ListView):
+    model = Reservations
+    context_object_name = "reserve"
+    template_name = "users/templates/users/home.php"
+    login_url = "login"
 
-class deleteReservation(LoginRequiredMixin,DeleteView):
-  model= Reservations
-  context_object_name = 'reserve'
-  success_url = 'main'
-  template_name = 'delete_reservation.php'
-  login_url = "login"
+
+class newReserveStaff(LoginRequiredMixin, CreateView):
+    model = Reservations
+    form_class = ReservationForm
+    success_url = "main"
+    template_name = "reservations_form.php"
+    login_url = "login"
+
+
+class deleteReservation(LoginRequiredMixin, DeleteView):
+    model = Reservations
+    context_object_name = "reserve"
+    success_url = "main"
+    template_name = "delete_reservation.php"
+    login_url = "login"
+
 
 class updateReserve(LoginRequiredMixin, UpdateView):
-  model = Reservations
-  form_class = ReservationForm
-  success_url = '/staff'
-  template_name = 'reserve_edit.php'
+    model = Reservations
+    form_class = ReservationForm
+    success_url = "/staff"
+    template_name = "reserve_edit.php"
