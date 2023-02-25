88605fd75ed5bd72df181fede0146601c24f279e
jcesguerra <jcesguerra@student.apc.edu.ph>
2023-02-25 12:48:00 +0800

    cleaned more pycaches

--- Reservation/urls.py
+++ Reservation/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-<<<<<<< HEAD
     path("test/", views.test, name="test"),
     path("reserve/", views.reserveView.as_view(), name="reserve"),
     path("reservation/<int:pk>", views.moreDetailView.as_view(), name="reservation"),
@@ -20,20 +19,7 @@ urlpatterns = [
     ),
     path("staff/reserve", views.newReserveStaff.as_view(), name="reserve.staff.new"),
     path("customer/new", views.newCustomer.as_view(), name="customer.new"),
+    path("price/new", views.addPrice.as_view(), name="price.new"),
+    path("price/edit/<int:pk>", views.editPrice.as_view(), name="price.edit"),
+    path("test/form", views.reserveNew, name="test1"),
 ]
-=======
-    path('test/', views.test,name='test'),
-    path('reserve/', views.reserveView.as_view(), name='reserve'),
-    path('reservation/<int:pk>', views.moreDetailView.as_view(),name = 'reservation'),
-    path('reservation/new', views.newReserve.as_view(), name='reservation.new'),
-    path('reservation/edit/<int:pk>', views.updateReserve.as_view(), name="reservation.edit"),
-    path('reservation/delete/<int:pk>', views.deleteReservation.as_view(), name='reservation.delete'),
-    path('staff/reserve', views.newReserveStaff.as_view(), name='reserve.staff.new'),
-    path('customer/new', views.newCustomer.as_view(), name='customer.new'),
-    path('price/new', views.addPrice.as_view(), name = 'price.new'),
-    path('price/edit/<int:pk>', views.editPrice.as_view(),name='price.edit'),
-    path('test/form', views.reserveNew, name="test1")
-
-
-]
->>>>>>> master
--- Reservation/views.py
+++ Reservation/views.py
@@ -16,7 +16,6 @@ from Reservation.models import Reservations, Customer, Discount, Prices
 from Reservation.forms import ReservationForm, CustomerForm, PriceForm
 from django.contrib.messages.views import SuccessMessageMixin
 
-
 # Create your views here.
 def test(request):
     return HttpResponse("this is a test")
@@ -27,66 +26,62 @@ class reserveView(ListView):
     context_object_name = "reserve"
     template_name = "reservation.php"
 
-
-  def get_context_data(self, **kwargs):
-    context = super(reserveView, self).get_context_data(**kwargs)
-    context['checkIn'] = Reservations.objects.values('checkIn')
-    context['checkOut'] = Reservations.objects.values('checkOut')
-    """ context['venue_list'] = Venue.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super(reserveView, self).get_context_data(**kwargs)
+        context["checkIn"] = Reservations.objects.values("checkIn")
+        context["checkOut"] = Reservations.objects.values("checkOut")
+        """ context['venue_list'] = Venue.objects.all()
     context['festival_list'] = Festival.objects.all()
     # And so on for more models """
-    return context
+        return context
+
 
 class newReserve(CreateView):
-<<<<<<< HEAD
     model = Reservations
     form_class = ReservationForm
+    form2 = CustomerForm
     success_url = "/reserve"
     template_name = "reservation_form_Customer.php"
 
-=======
-  model = Reservations
-  form_class = ReservationForm
-  form2 = CustomerForm
-  success_url = '/reserve'
-  template_name = 'reservation_form_Customer.php'
->>>>>>> master
-
-  def get_context_data(self, **kwargs):
-    context = super(newReserve, self).get_context_data(**kwargs)
-    context['prices'] = Prices.objects.all().values()
-    context['checkOut'] = Reservations.objects.values('checkOut')
-    """ context['venue_list'] = Venue.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super(newReserve, self).get_context_data(**kwargs)
+        context["prices"] = Prices.objects.all().values()
+        context["checkOut"] = Reservations.objects.values("checkOut")
+        """ context['venue_list'] = Venue.objects.all()
     context['festival_list'] = Festival.objects.all()
     # And so on for more models """
-    return context
+        return context
+
 
 def reserveNew(request):
-  obj = Customer.objects.values()
-  form = ReservationForm(request.POST or None)
-  form2 = CustomerForm(request.POST or None)
-  context={
-    "form": form,
-    "form_2":form2,
-    "object":obj,   
-    "prices":Prices.objects.all().values()
-  }
+    obj = Customer.objects.values()
+    form = ReservationForm(request.POST or None)
+    form2 = CustomerForm(request.POST or None)
+    context = {
+        "form": form,
+        "form_2": form2,
+        "object": obj,
+        "prices": Prices.objects.all().values(),
+    }
 
-  if all([form.is_valid(), form2.is_valid()]):
-    parent=form2.save(commit=False)
-    child=form.save(commit=False)
-    child.customer = parent
-    print(form2.cleaned_data)
-    print(form.cleaned_data)
-    """ form2.save()
-    form.save() """
-    for x in obj:
-      if form2.cleaned_data['firstname'].lower() == x['firstname'].lower() and form2.cleaned_data['lastname'].lower() == x['lastname'].lower() and form2.cleaned_data['email'].lower() == x['email'].lower():
-        print(x['firstname'])
-        form.cleaned_data['customer_id'] = x['id']
+    if all([form.is_valid(), form2.is_valid()]):
+        parent = form2.save(commit=False)
+        child = form.save(commit=False)
+        child.customer = parent
+        print(form2.cleaned_data)
         print(form.cleaned_data)
-  return render(request, 'reservation_form_Customer.php', context)
-
+        """ form2.save()
+    form.save() """
+        for x in obj:
+            if (
+                form2.cleaned_data["firstname"].lower() == x["firstname"].lower()
+                and form2.cleaned_data["lastname"].lower() == x["lastname"].lower()
+                and form2.cleaned_data["email"].lower() == x["email"].lower()
+            ):
+                print(x["firstname"])
+                form.cleaned_data["customer_id"] = x["id"]
+                print(form.cleaned_data)
+    return render(request, "reservation_form_Customer.php", context)
 
 
 class newCustomer(CreateView):
@@ -126,30 +121,27 @@ class deleteReservation(LoginRequiredMixin, DeleteView):
 
 
 class updateReserve(LoginRequiredMixin, UpdateView):
-<<<<<<< HEAD
     model = Reservations
     form_class = ReservationForm
     success_url = "/staff"
     template_name = "reserve_edit.php"
-=======
-  model = Reservations
-  form_class = ReservationForm
-  success_url = '/staff'
-  template_name = 'reserve_edit.php'
+
 
 class addPrice(LoginRequiredMixin, CreateView):
-  model = Prices
-  form_class = PriceForm
-  success_url = '/staff/'
-  template_name = 'prices_new.php'
+    model = Prices
+    form_class = PriceForm
+    success_url = "/staff/"
+    template_name = "prices_new.php"
+
+
 class editPrice(LoginRequiredMixin, UpdateView):
-  model = Prices
-  form_class = PriceForm
-  success_url = '/staff/'
-  template_name = 'prices_new.php'
+    model = Prices
+    form_class = PriceForm
+    success_url = "/staff/"
+    template_name = "prices_new.php"
+
 
 """ class DiscountView(ListView):
   model = Discount
   template_name = 'discount_view.php'
    """
->>>>>>> master
