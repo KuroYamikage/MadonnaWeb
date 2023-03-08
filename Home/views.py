6245b85c7369da077e6b52329fe8515dbe891531
jcesguerra <jcesguerra@student.apc.edu.ph>
2023-03-08 11:06:28 +0800

    laksjdlksad

--- Home/views.py
+++ Home/views.py
@@ -3,96 +3,121 @@ from django.core import serializers
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.template import loader
-from django.views.generic import CreateView, TemplateView,ListView, DetailView, UpdateView
+from django.views.generic import (
+    CreateView,
+    TemplateView,
+    ListView,
+    DetailView,
+    UpdateView,
+)
 from django.views.generic.edit import DeleteView
 from Reservation.models import Reservations, Facility
-<<<<<<< HEAD
-from blog.models import Blog
-=======
 from blog.models import Blog, Gallery
->>>>>>> Reservation
 
 
+# Blog Files
 
-#Blog Files
+# Reservation Files
 
-#Reservation Files
 
-<<<<<<< HEAD
-class GalleryView(TemplateView):
-=======
 class GalleryView(ListView):
     model = Gallery
-    context_object_name = 'gallery'
->>>>>>> Reservation
+    context_object_name = "gallery"
     template_name = "gallery.php"
 
-class detailGallery (DetailView):
-  model = Gallery
-  context_object_name= 'gallery'
-  template_name = 'gallery_detail.php'
+
+class detailGallery(DetailView):
+    model = Gallery
+    context_object_name = "gallery"
+    template_name = "gallery_detail.php"
+
+
 class sampleView(ListView):
-  model = Reservations
-  context_object_name = "reserve"
-  template_name='test.php'
-  
+    model = Reservations
+    context_object_name = "reserve"
+    template_name = "test.php"
 
 
-
-
-#Home Page Views
+# Home Page Views
 class indexView(TemplateView):
-  template_name='index.php'
-  context_object_name = 'home'
+    template_name = "index.php"
+    context_object_name = "home"
 
-  def get_context_data(self, **kwargs):
-    context = super(indexView, self).get_context_data(**kwargs)
-    context['blog'] = Blog.objects.values()
-    context['pool1'] = Facility.objects.values_list('id','facilityPic','facilityName')
+    def get_context_data(self, **kwargs):
+        context = super(indexView, self).get_context_data(**kwargs)
+        context["blog"] = Blog.objects.values()
+        context["pool1"] = Facility.objects.values_list(
+            "id", "facilityPic", "facilityName"
+        )
 
-    """ context['venue_list'] = Venue.objects.all()
+        """ context['venue_list'] = Venue.objects.all()
     context['festival_list'] = Festival.objects.all()
     # And so on for more models """
-    return context
-
+        return context
 
 
 class aboutView(TemplateView):
-  template_name='about.php'
+    template_name = "about.php"
 
 
 class addView(TemplateView):
-  template_name = "add.php"
+    template_name = "add.php"
+
 
 class facilitiesView(ListView):
-  model = Facility
-  template_name = 'facilities.php'
-  context_object_name = 'facility'
+    model = Facility
+    template_name = "facilities.php"
+    context_object_name = "facility"
 
 
 def reservations(request):
-  reserve = Reservations.objects.all().order_by('reservationID')
-  template = loader.get_template('reservation.php')
-  reserve_html = []
-  for instance in Reservations.objects.all():  # it's not serialization, but extracting of the useful fields
-      reserve_html.append({'pk': instance.reservationID, 'fname': instance.firstname, 'lname': instance.lastname, 
-      'date': instance.date, 'dp': instance.downpayment, 'total': instance.totalPayment, 'bal': instance.balance
-      , 'stat': instance.status})
-  reserve_dic = {'reserve': reserve, 'ac_tab_n': 'ac_tab', 'reserve_html': reserve_html}
-  return render(request, 'reservation.php', reserve_dic)
+    reserve = Reservations.objects.all().order_by("reservationID")
+    template = loader.get_template("reservation.php")
+    reserve_html = []
+    for (
+        instance
+    ) in (
+        Reservations.objects.all()
+    ):  # it's not serialization, but extracting of the useful fields
+        reserve_html.append(
+            {
+                "pk": instance.reservationID,
+                "fname": instance.firstname,
+                "lname": instance.lastname,
+                "date": instance.date,
+                "dp": instance.downpayment,
+                "total": instance.totalPayment,
+                "bal": instance.balance,
+                "stat": instance.status,
+            }
+        )
+    reserve_dic = {
+        "reserve": reserve,
+        "ac_tab_n": "ac_tab",
+        "reserve_html": reserve_html,
+    }
+    return render(request, "reservation.php", reserve_dic)
 
 
 def addrecord(request):
-  fname = request.POST['first']
-  lname = request.POST['last']
-  date = request.POST['date']   
-  dp = request.POST['dp']
-  tp = request.POST['tp']
-  bal = request.POST['bal']
-  stat=request.POST['status']
-  newRec = Reservations(firstname=fname, lastname=lname, date=date, downpayment=dp, totalPayment=tp, balance=bal, status=stat )
-  newRec.save()
-  return HttpResponseRedirect(reverse('sample'))
+    fname = request.POST["first"]
+    lname = request.POST["last"]
+    date = request.POST["date"]
+    dp = request.POST["dp"]
+    tp = request.POST["tp"]
+    bal = request.POST["bal"]
+    stat = request.POST["status"]
+    newRec = Reservations(
+        firstname=fname,
+        lastname=lname,
+        date=date,
+        downpayment=dp,
+        totalPayment=tp,
+        balance=bal,
+        status=stat,
+    )
+    newRec.save()
+    return HttpResponseRedirect(reverse("sample"))
+
+
 # Create your views here.
-
-
