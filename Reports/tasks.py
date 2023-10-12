caf6dde5d4fb75a848db2af00f3d5952a983c8ed
jcesguerra <jcesguerra@student.apc.edu.ph>
2023-10-13 00:32:42 +0800

    Implementing end of month tasks for Reports

---
+++ Reports/tasks.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import calendar
+from celery import shared_task
+from .scripts.reports import month
+from .models import MonthReport
+
+
+@shared_task
+def generate_monthly_report():
+    current_year = datetime.now().year
+    current_month = datetime.now().month
+    end_of_month = calendar.monthrange(current_year, current_month)[1]
+    today = datetime.now().day
+
+    if today == end_of_month:
+        _, total_earnings, total_reservations = month()
+
+        monthly_report = MonthReport(
+            report_month=current_month,
+            total_earnings=total_earnings,
+            total_reservations=total_reservations,
+        )
